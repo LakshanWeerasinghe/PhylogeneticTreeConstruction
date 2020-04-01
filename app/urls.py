0be9d87aea238b7884330173dfef7dff556df283
LakshanWeerasinghe <lak98weerasinghe@gmail.com>
2020-04-01 19:01:56 +0530

    static files added to the url

--- app/urls.py
+++ app/urls.py
@@ -15,11 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     
     path('admin/', admin.site.urls),
     path('file_system/', include('file_upload.urls')),
     path('users/', include('users.urls')),
-
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
