5d038784d2fa94d63a443f4229589f6ada19d6fd
GihanAyesh <gihanayeshmanthadrck@gmail.com>
2020-04-22 15:20:04 +0530

    minor bug fixes

--- dna_storage/urls.py
+++ dna_storage/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
-<<<<<<< HEAD
-from django.conf.urls import url
-from .views import get_dna_sequence_upload_url_view, dna_file_uploaded_view
-from . import views
-=======
 from .views import *
->>>>>>> c2e7188f07e30e02d8657cf1aa0d325a3b056f87
+
 
 """
     url patterns are :
@@ -14,8 +9,7 @@ from .views import *
 """
 
 urlpatterns = [
-    #/music/
-    url(r'^$', views.IndexView.as_view(),name='index'), 
+
 
     path('upload/', get_dna_sequence_upload_url_view, name='upload'),
     path('updateStatus/', dna_file_uploaded_view, name="updateStatus"),
