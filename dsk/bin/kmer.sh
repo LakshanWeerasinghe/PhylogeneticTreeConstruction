9a68dcbe8740d6546551baa1e9f7162a1a93e5e6
LakshanWeerasinghe <lak98weerasinghe@gmail.com>
2020-04-26 15:47:27 +0530

    new docker file

--- dsk/bin/kmer.sh
+++ dsk/bin/kmer.sh
@@ -4,20 +4,28 @@
 GENOME_DIRECTORY=$1
 OUTPUT_DIRECTORY=$2
 
+dir=/usr/src/app/dsk/bin/
+for file in "$dir/"*
+do
+  [[ -d "$file" ]] && echo "$file is a directory"
+  [[ -f "$file" ]] && echo "$file is a regular file"
+done
 
 for file in ${GENOME_DIRECTORY}/*.fna; do
      fileName="$(basename -- $file)"
      specieName="${fileName%.*}"
      outputH5Name=${specieName}"_output"
      outputKmerSpecieName=${specieName}"_kmer_output" 		
-     echo ${fileName}"_Kmer counting started.."		 		
+     echo ${fileName} " Kmer Counting Started "		 		
      
      # Change max-memory and kmer-size parameters accordingly	
-     ./dsk -nb-cores 1 -max-memory 300 -file ${GENOME_DIRECTORY}/${fileName} -kmer-size 11 -out ${OUTPUT_DIRECTORY}/${outputH5Name}
+     ./dsk/bin/dsk -nb-cores 1 -max-memory 300 -file ${GENOME_DIRECTORY}/${fileName} -kmer-size 11 -out ${OUTPUT_DIRECTORY}/${outputH5Name}
      ./dsk2ascii -file ${OUTPUT_DIRECTORY}/${outputH5Name} -out ${OUTPUT_DIRECTORY}/${outputKmerSpecieName}		  	
-     echo ${fileName}"_Kmer counting Finished.."	
+     echo ${fileName} " Kmer counting finished "
 done
 for file1 in ${OUTPUT_DIRECTORY}*.h5; do   
-     echo ${file1} " Deleted"
+     echo ${file1} " Deleted "
      rm -f ${file1}	
 done
+
+exit 1
--- dsk/bin/kp.py
+++ dsk/bin/kp.py
@@ -1,9 +1,41 @@
 import os
+from subprocess import Popen, PIPE, STDOUT
 import subprocess
 
 
 def run_kmer_sh_file(dna_sequence_path, dsk_results_path):
 
-    s = subprocess.Popen(args=["/usr/src/app/dsk/bin/kmer.sh %s %s" %
-                               (dna_sequence_path, dsk_results_path)], shell=True)
-    print("done")
+    command = ["bash", "dsk/bin/kmer.sh",
+               dna_sequence_path, dsk_results_path]
+    try:
+        process = Popen(command, stdout=PIPE, stderr=STDOUT)
+        output = process.stdout.read()
+        exitstatus = process.poll()
+        print(output)
+    except Exception as e:
+        print(str(e))
+
+    print(subprocess.call(["./dsk2ascii"], shell=True))
+
+    # print("Finished")
+
+    try:
+        cmd = './dsk'
+        so = os.popen(cmd).read()
+        print(so)
+    except Exception as e:
+        print(e)
+
+    try:
+        cmd = './dsk2ascii'
+        so = os.popen(cmd).read()
+        print(so)
+    except Exception as e:
+        print(e)
+
+    # try:
+    #     cmd = 'ls -l dsk/bin'
+    #     so = os.popen(cmd).read()
+    #     print(so)
+    # except Exception as e:
+    #     print(e)
