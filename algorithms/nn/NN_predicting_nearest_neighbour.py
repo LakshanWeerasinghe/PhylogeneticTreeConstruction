3aff5380db4b5a467a132488e5eab29757d2dda3
GihanAyesh <gihanayeshmanthadrck@gmail.com>
2020-04-29 13:29:48 +0530

    turning algorithms to functions

--- algorithms/nn/NN_predicting_nearest_neighbour.py
+++ algorithms/nn/NN_predicting_nearest_neighbour.py
@@ -9,100 +9,92 @@ from sklearn.metrics import mean_squared_error
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-
-
-train_df = pd.read_csv('Training_Data.csv')
-def norm(x,stats):
-  return (x - stats['mean']) / stats['std']
-train_stats = {'mean':train_df.mean(),'std': train_df.std()}
-normed_train_data = norm(train_df,train_stats)
-
-train_X = normed_train_data.drop('kmerSimilarity', axis=1).values
-
-train_Y = normed_train_data['kmerSimilarity'].values
-
-X_train, X_test, y_train, y_test = train_test_split(train_X,train_Y,test_size=0.001)
-
-
-print(type(X_test))
-#random_state=6,21,8
-# testData_with_names = pd.read_csv('testDatasetDeleted60_nonames.csv')
-
-# print(train_df)
 import keras.backend as K
 import tensorflow as tf
-accepted_diff = 0.09
-def linear_regression_equality(y_true, y_pred):
-    diff = K.abs(y_true-y_pred)
-    return K.mean(K.cast(diff < accepted_diff, tf.float32))
+
+def predict_nearest_neighbour(training_data): #'Training_Data.csv'
+    train_df = pd.read_csv(training_data)
+    def norm(x,stats):
+        return (x - stats['mean']) / stats['std']
+    train_stats = {'mean':train_df.mean(),'std': train_df.std()}
+    normed_train_data = norm(train_df,train_stats)
+
+    train_X = normed_train_data.drop('kmerSimilarity', axis=1).values
+
+    train_Y = normed_train_data['kmerSimilarity'].values
+
+    X_train, X_test, y_train, y_test = train_test_split(train_X,train_Y,test_size=0.001)
 
 
-targetLSH = 'LSH Similarity'
-target = 'kmerSimilarity'
+    print(type(X_test))
+    #random_state=6,21,8
+    # testData_with_names = pd.read_csv('testDatasetDeleted60_nonames.csv')
 
-train_stats = {'mean':X_train.mean(),'std': X_train.std()}
-test_stats = {'mean':X_test.mean(),'std': X_test.std()}
+    # print(train_df)
 
-print('test_stat',test_stats)
-print('train_stat',train_stats)
-# def norm(x,stats):
-#   return (x - stats['mean']) / stats['std']
-# normed_train_data = norm(X_train,train_stats)
-# normed_test_data = norm(X_test,train_stats)
+    accepted_diff = 0.09
+    def linear_regression_equality(y_true, y_pred):
+        diff = K.abs(y_true-y_pred)
+        return K.mean(K.cast(diff < accepted_diff, tf.float32))
 
 
-model = Sequential()
-# model.add(Dropout(0.2, input_shape=(36,)))
-model.add(Dense(100, activation='relu',input_shape=(36,)))
-model.add(Dense(50, activation='relu'))
+    targetLSH = 'LSH Similarity'
+    target = 'kmerSimilarity'
 
-model.add(Dense(1))
+    train_stats = {'mean':X_train.mean(),'std': X_train.std()}
+    test_stats = {'mean':X_test.mean(),'std': X_test.std()}
 
-model.compile(loss='mean_squared_error', optimizer='adam', metrics=[linear_regression_equality])
-# train_X = normed_train_data.drop(target, axis=1).values
-#
-# train_Y = normed_train_data[target].values
+    print('test_stat',test_stats)
+    print('train_stat',train_stats)
+    # def norm(x,stats):
+    #   return (x - stats['mean']) / stats['std']
+    # normed_train_data = norm(X_train,train_stats)
+    # normed_test_data = norm(X_test,train_stats)
 
-model.fit(
-    X_train,
-    y_train,
-    epochs=50,
-    verbose=2
-)
+    model = Sequential()
+    # model.add(Dropout(0.2, input_shape=(36,)))
+    model.add(Dense(100, activation='relu',input_shape=(36,)))
+    model.add(Dense(50, activation='relu'))
 
+    model.add(Dense(1))
 
-# test_X = normed_test_data.drop(target, axis=1).values
-# test_Y = normed_test_data[target].values
+    model.compile(loss='mean_squared_error', optimizer='adam', metrics=[linear_regression_equality])
+    # train_X = normed_train_data.drop(target, axis=1).values
+    #
+    # train_Y = normed_train_data[target].values
 
-print("evaluate")
-results = model.evaluate(X_test,y_test)
-print(results)
+    model.fit(
+        X_train,
+        y_train,
+        epochs=50,
+        verbose=2
+    )
 
+    # test_X = normed_test_data.drop(target, axis=1).values
+    # test_Y = normed_test_data[target].values
 
-model_json = model.to_json()
-with open ('model.json','w') as json_file:
-    json_file.write(model_json)
-model.save_weights("model.h5")
-print("Saved model to disk")
+    print("evaluate")
+    results = model.evaluate(X_test,y_test)
+    print(results)
 
+    model_json = model.to_json()
+    with open ('model.json','w') as json_file:
+        json_file.write(model_json)
+    model.save_weights("model.h5")
+    print("Saved model to disk")
 
+    prediction = model.predict(X_test)
+    print(prediction)
 
-prediction = model.predict(X_test)
-print(prediction)
+    predictionArray=[]
 
+    for i in prediction:
+        predictionArray.append(i[0])
+    print("predicted max",predictionArray.index(max(predictionArray)))
+    print("ground truth max index",y_test.tolist().index(max(y_test)))
 
-
-
-
-predictionArray=[]
-
-for i in prediction:
-    predictionArray.append(i[0])
-print("predicted max",predictionArray.index(max(predictionArray)))
-print("ground truth max index",y_test.tolist().index(max(y_test)))
-
-plt.plot(predictionArray)
-# plt.show()
-plt.plot(y_test)
-# plt.plot(Y)
-plt.show()
+    plt.plot(predictionArray)
+    # plt.show()
+    plt.plot(y_test)
+    # plt.plot(Y)
+    plt.show()
--- algorithms/nn/Update_tree.py
+++ algorithms/nn/Update_tree.py
@@ -10,77 +10,79 @@ import numpy as np
 import tensorflow as tf
 
 from keras.models import model_from_json
-json_file = open('model.json','r')
-accepted_diff = 0.09
-def linear_regression_equality(y_true, y_pred):
-    diff = K.abs(y_true-y_pred)
-    return K.mean(K.cast(diff < accepted_diff, tf.float32))
-loaded_model_json = json_file.read()
+
+def update_tree(json_object):
+  json_file = open(json_object,'r')
+  accepted_diff = 0.09
+  def linear_regression_equality(y_true, y_pred):
+      diff = K.abs(y_true-y_pred)
+      return K.mean(K.cast(diff < accepted_diff, tf.float32))
+  loaded_model_json = json_file.read()
 
 
-json_file.close()
+  json_file.close()
 
-loaded_model = model_from_json(loaded_model_json)
-loaded_model.load_weights("model.h5")
-print("Loaded model from disk")
+  loaded_model = model_from_json(loaded_model_json)
+  loaded_model.load_weights("model.h5")
+  print("Loaded model from disk")
 
-loaded_model.compile(loss='mean_squared_error', optimizer='adam', metrics=[linear_regression_equality])
+  loaded_model.compile(loss='mean_squared_error', optimizer='adam', metrics=[linear_regression_equality])
 
-predict_data = pd.read_csv('Prediction_Data.csv')
-
-
-New_specie = predict_data['DNA 1'].values[0]
-Existing_species = predict_data['DNA 2'].values
+  predict_data = pd.read_csv('Prediction_Data.csv')
 
 
-predict_data = predict_data.drop('DNA 1', axis=1)
-predict_data = predict_data.drop('DNA 2', axis=1)
+  New_specie = predict_data['DNA 1'].values[0]
+  Existing_species = predict_data['DNA 2'].values
 
 
-def norm(x,stats):
-  return (x - stats['mean']) / stats['std']
-
-predicting_stats = {'mean':predict_data.mean(),'std': predict_data.std()}
-# print(predicting_stats)
-
-normed_predict_data = norm(predict_data,predicting_stats)
-# print(normed_predict_data.head())
-
-# print(New_specie,Existing_species)
-prediction = loaded_model.predict(normed_predict_data)
-predited_similarities = []
-for each in prediction:
-    predited_similarities.append(each)
-
-# print( Existing_species[predited_similarities.index(max(predited_similarities))])
-
-nearest_neighbour = Existing_species[predited_similarities.index(max(predited_similarities))]
-
-print("Nearest Neighbour for the new specie",New_specie," is ",nearest_neighbour)
+  predict_data = predict_data.drop('DNA 1', axis=1)
+  predict_data = predict_data.drop('DNA 2', axis=1)
 
 
+  def norm(x,stats):
+    return (x - stats['mean']) / stats['std']
 
-# tree updation visualization
-with open('../kmedoid/kmer_tree.json') as f:
-  data = json.load(f)
+  predicting_stats = {'mean':predict_data.mean(),'std': predict_data.std()}
+  # print(predicting_stats)
 
-# print data;
-parent = {}
-def find_nn(data,parent,nearest_neighbour):
-  # print data['name']
-  if(data['name']==nearest_neighbour):
-    print ("found")
-    data['children'] = [{'name': nearest_neighbour},{'name':New_specie}]
-    data['name'] = ''
-  else:
-    if('children' in data.keys()):
-      find_nn(data['children'][0],data,nearest_neighbour)
-      find_nn(data['children'][1],data, nearest_neighbour)
+  normed_predict_data = norm(predict_data,predicting_stats)
+  # print(normed_predict_data.head())
+
+  # print(New_specie,Existing_species)
+  prediction = loaded_model.predict(normed_predict_data)
+  predited_similarities = []
+  for each in prediction:
+      predited_similarities.append(each)
+
+  # print( Existing_species[predited_similarities.index(max(predited_similarities))])
+
+  nearest_neighbour = Existing_species[predited_similarities.index(max(predited_similarities))]
+
+  print("Nearest Neighbour for the new specie",New_specie," is ",nearest_neighbour)
 
 
-find_nn(data,{},nearest_neighbour)
-with open('updated_tree.json', 'w') as outfile:
-  json.dump(data, outfile)
 
-filename = os.getcwd()+'/' + 'Updated_Tree_visualization.html'
-webbrowser.open_new_tab(filename)
+  # tree updation visualization
+  with open('../kmedoid/kmer_tree.json') as f:
+    data = json.load(f)
+
+  # print data;
+  parent = {}
+  def find_nn(data,parent,nearest_neighbour):
+    # print data['name']
+    if(data['name']==nearest_neighbour):
+      print ("found")
+      data['children'] = [{'name': nearest_neighbour},{'name':New_specie}]
+      data['name'] = ''
+    else:
+      if('children' in data.keys()):
+        find_nn(data['children'][0],data,nearest_neighbour)
+        find_nn(data['children'][1],data, nearest_neighbour)
+
+
+  find_nn(data,{},nearest_neighbour)
+  with open('updated_tree.json', 'w') as outfile:
+    json.dump(data, outfile)
+
+  filename = os.getcwd()+'/' + 'Updated_Tree_visualization.html'
+  webbrowser.open_new_tab(filename)
